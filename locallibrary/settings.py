a348dd853de9308c1d69fab16fffbc181f4b98df
Andrea <andrealh6666@gmail.com>
2024-01-20 15:55:32 +0100

    locallibrary con postgres y su configuracion

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -176,4 +176,4 @@ elif stage == "development":
     from .settings_dev import *
 else:
     # Arrojar un error si MYPROJECT_STAGE tiene un valor desconocido.
-    raise ValueError("Unknown stage: {stage}")
+    raise ValueError(f"Unknown stage: {stage}")
